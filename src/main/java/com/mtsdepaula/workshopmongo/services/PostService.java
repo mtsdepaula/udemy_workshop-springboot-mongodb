b1e2d99364a7a83ff1efabef0438abe36bbf6356
mtsdepaula <mtsdepaula@gmail.com>
2023-06-19 16:31:33 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mtsdepaula/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mtsdepaula.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mtsdepaula.workshopmongo.domain.Post;
+import com.mtsdepaula.workshopmongo.repository.PostRepository;
+import com.mtsdepaula.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
