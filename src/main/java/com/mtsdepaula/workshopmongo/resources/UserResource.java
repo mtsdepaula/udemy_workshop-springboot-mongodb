ae25160f810dfb587b23657dc540eadd2eda6299
mtsdepaula <mtsdepaula@gmail.com>
2023-06-19 16:21:14 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/mtsdepaula/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mtsdepaula/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mtsdepaula.workshopmongo.domain.Post;
 import com.mtsdepaula.workshopmongo.domain.User;
 import com.mtsdepaula.workshopmongo.dto.UserDTO;
 import com.mtsdepaula.workshopmongo.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
