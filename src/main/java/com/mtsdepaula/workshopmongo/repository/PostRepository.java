e497a76c180ceae74afd8e26f64163719f6450b9
mtsdepaula <mtsdepaula@gmail.com>
2023-06-20 15:47:54 -0300

    Consulta simples com query methods

--- src/main/java/com/mtsdepaula/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mtsdepaula/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.mtsdepaula.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.mtsdepaula.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoringCase(String text);
 }
